99c3912720159c59986a91b7d28d254a4ef4f2ee
hetamotor <sue.deng@hetamotor.com>
2020-03-21 06:40:22 +0800

    Adjust the GUI and add some TODO

--- src/main/java/com/ht/jna/KeySightDevice_Electricity.java
+++ src/main/java/com/ht/jna/KeySightDevice_Electricity.java
@@ -1,10 +1,15 @@
 package com.ht.jna;
 
+import com.ht.base.SpringContext;
+import com.ht.entity.Devices;
+import com.ht.repository.DevicesRepo;
 import com.sun.jna.Memory;
 import com.sun.jna.NativeLong;
 import com.sun.jna.ptr.LongByReference;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.util.Optional;
 
 public class KeySightDevice_Electricity {
     private static final Log logger = LogFactory.getLog(KeySightDevice_Electricity.class);
@@ -36,13 +41,19 @@ public class KeySightDevice_Electricity {
         // defaultSession = new LongByReference(getStartInf());
 
         try {
-            /*String path = "keysight.xlsx";
-            InputStream inputStream = null;
-            inputStream = new FileInputStream(path);
-            Workbook workbook = WorkbookFactory.create(inputStream);
-            Sheet sheet = workbook.getSheet("IP");
-            String eleIp = sheet.getRow(0).getCell(1).getStringCellValue();*/
-            String eleIp = "169.254.210.1";
+            /*****
+             从数据库获得IP地址
+             界面上不能设置IP，以后要改，就直接改数据库
+             *****/
+            String eleIp = DefaultDevicesConfig.eleIP;
+            try {
+                DevicesRepo repo = SpringContext.getBean(DevicesRepo.class);
+                Optional<Devices> device = repo.findById(DefaultDevicesConfig.eleName);
+                eleIp = device.get().getIpAddress();
+            } catch (Exception e) {
+                logger.error(e);
+            }
+
             defaultSession = new LongByReference(59005407);
             int result = KEYSIGHTINSTANCE.viOpenDefaultRM(defaultSession);
             if (result != KEYSIGHTINSTANCE.STATUS_OK) {
@@ -50,8 +61,6 @@ public class KeySightDevice_Electricity {
             }
             vipSession = new LongByReference(0);
             String cmd = "TCPIP0::" + eleIp + "::inst0::INSTR";
-            // String cmd = "TCPIP0::192.168.0.120::5024::SOCKET";
-            // String cmd = "USB0::0x2A8D::0x1301::MY59000220::0::INSTR";
             NativeLong a = new NativeLong(defaultSession.getValue());
             NativeLong b = new NativeLong(0);
             result = KEYSIGHTINSTANCE.viOpen(a, cmd, b, b, vipSession);
@@ -59,8 +68,13 @@ public class KeySightDevice_Electricity {
                 return false;
             }
             logger.info("连接ip=" + eleIp + "的设备成功");
+
+            // TODO: 界面上，此设备状态设为Green
+            // TODO: 界面上，TextArea增加一行"电流万用表连接成功"
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e);
+            // TODO: 界面上，此设备状态设为Red
+            // TODO: 通知主控，设备有错
         }
         isOpened = true;
         return true;
@@ -75,11 +89,16 @@ public class KeySightDevice_Electricity {
         }
         NativeLong b = new NativeLong(defaultSession.getValue());
         result = KEYSIGHTINSTANCE.viClose(b);
+
+        // TODO: 退出远程模式失败，界面上要显示不？
         if (result != KEYSIGHTINSTANCE.STATUS_OK) {
             logger.warn("KeySight退出远程模式失败");
             return false;
         }
+
         logger.info("KeySight退出远程模式成功");
+        // TODO: 界面上TEXTAREA上显示"KeySight退出远程模式成功"
+        // TODO: 界面上此设备状态显示为"--"
         return true;
     }
 
@@ -106,6 +125,8 @@ public class KeySightDevice_Electricity {
         int result = KEYSIGHTINSTANCE.viPrintf(a, "%s\n", cmdStr);
         if (result != KEYSIGHTINSTANCE.STATUS_OK) {
             logger.warn("执行命令失败,result=" + result);
+            // TODO: 界面上TEXTAREA上显示"'执行命令失败,result=' + result"
+            // TODO: 界面上此设备状态显示为Red
         }
         return true;
     }
@@ -116,6 +137,8 @@ public class KeySightDevice_Electricity {
         int result = KEYSIGHTINSTANCE.viScanf(a, "%t", mem);
         if (result != KEYSIGHTINSTANCE.STATUS_OK) {
             logger.info(result);
+            // TODO: 界面上TEXTAREA上显示"'执行命令失败,result=' + result"
+            // TODO: 界面上此设备状态显示为Red
             return null;
         }
         return mem.getString(0);
